ada0ff20384e741ef77f63bc8714a7ae161946a9
k3tmoraes <ketlen_furtado@estudante.sesisenai.org.br>
2025-05-26 22:20:15 -0300

    feat: Primeiro heact

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo02'>Dinos são legais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis velit totam numquam! Nobis quis veritatis atque a voluptates obcaecati, libero repudiandae maiores quasi laborum suscipit nulla vel veniam nisi sapiente.</p>
+    </div>
+  )
+}
+
+export default Body
